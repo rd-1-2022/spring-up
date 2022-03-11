6ea6f51ab5a7af281fd03bc3ebf751cc189f96cc
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-03-11 07:56:46 +0000

    Remove JavaBeanTableCreator
    - In favour of keeping things light use array
      and things from spring-shell.

--- src/main/java/org/springframework/up/command/BootTemplateCommands.java
+++ src/main/java/org/springframework/up/command/BootTemplateCommands.java
@@ -13,41 +13,44 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.springframework.up.command;
 
-import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.ArrayTableModel;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
 import org.springframework.up.config.TemplateRepository;
 import org.springframework.up.config.UpCliProperties;
-import org.springframework.up.util.JavaBeanTableCreator;
+import org.springframework.util.StringUtils;
 
 @ShellComponent
 public class BootTemplateCommands {
 
 	private UpCliProperties upCliProperties;
 
-	private JavaBeanTableCreator javaBeanTableCreator;
-
 	@Autowired
-	public BootTemplateCommands(UpCliProperties upCliProperties, JavaBeanTableCreator javaBeanTableCreator) {
+	public BootTemplateCommands(UpCliProperties upCliProperties) {
 		this.upCliProperties = upCliProperties;
-		this.javaBeanTableCreator = javaBeanTableCreator;
 	}
 
 	@ShellMethod(key = "template list", value = "List templates available to create a new Spring Boot project")
-	public void templateList() {
-		// TODO Handle the catalog later
+	public Table templateList() {
+		Stream<String[]> header = Stream.<String[]>of(new String[] { "Name", "Description", "Tags" });
 		List<TemplateRepository> templateRepositories = this.upCliProperties.getTemplateRepositories();
-		LinkedHashMap<String, Object> headers = new LinkedHashMap<>();
-		headers.put("name", "Name");
-		headers.put("description", "Description");
-		headers.put("tags", "Tags");
-		this.javaBeanTableCreator.createTable(templateRepositories, headers);
+		Stream<String[]> rows = templateRepositories.stream()
+				.map(tr -> new String[] { tr.getName(), tr.getDescription(),
+						StringUtils.collectionToCommaDelimitedString(tr.getTags()) });
+		String[][] data = Stream.concat(header, rows).toArray(String[][]::new);
+
+		TableModel model = new ArrayTableModel(data);
+		TableBuilder tableBuilder = new TableBuilder(model);
+		return tableBuilder.addFullBorder(BorderStyle.fancy_light).build();
 	}
 }
